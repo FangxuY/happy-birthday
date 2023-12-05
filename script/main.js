58e83498f35ee11266e4d0a1081ad3708237c7b1
Fangxu Yuan <48797491+FangxuY@users.noreply.github.com>
2023-12-05 11:32:22 +0000

    change hat position

--- script/main.js
+++ script/main.js
@@ -221,8 +221,8 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -150,
-      y: 400,
+      x: -100,
+      y: 450,
       rotation: -180,
       opacity: 0
     })
